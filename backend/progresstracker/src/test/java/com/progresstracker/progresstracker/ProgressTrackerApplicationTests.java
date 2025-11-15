c360d6e41170e5fe9a5c07d1d9647f19d457824d
Andrew Sou <andrewsou4@gmail.com>
2025-11-15 03:00:07 -0800

    Setup project and backend working with database

---
+++ backend/progresstracker/src/test/java/com/progresstracker/progresstracker/ProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresstracker.progresstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
