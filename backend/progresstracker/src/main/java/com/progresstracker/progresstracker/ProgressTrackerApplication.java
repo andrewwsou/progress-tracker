0015c7efa7e0bc980e5f982e4ea0e3a0df66ac3f
Andrew Sou <aksou@uci.edu>
2025-11-15 03:00:07 -0800

    Setup project and backend working with database

---
+++ backend/progresstracker/src/main/java/com/progresstracker/progresstracker/ProgressTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.progresstracker.progresstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProgressTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackerApplication.class, args);
+	}
+
+}
