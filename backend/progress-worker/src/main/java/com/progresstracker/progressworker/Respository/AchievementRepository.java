94f44722806ef87d52bab5558818fa8f85c7a8b3
Andrew Sou <andrewsou4@gmail.com>
2026-01-14 13:40:53 -0800

    Added achievement repos to find achievements

---
+++ backend/progress-worker/src/main/java/com/progresstracker/progressworker/Respository/AchievementRepository.java
@@ -0,0 +1,10 @@
+package com.progresstracker.progressworker.repository;
+
+import com.progresstracker.progressworker.model.Achievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+    Optional<Achievement> findByCode(String code);
+}
